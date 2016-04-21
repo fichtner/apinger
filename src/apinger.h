aced2a977ee2a4dde6933829dd16b71bd78e6c14
Franco Fichtner <franco@lastsummer.de>
2016-04-21 20:21:26 +0200

    apply FreeBSD ports patches
    Taken from: FreeBSD

--- src/apinger.h
+++ src/apinger.h
@@ -118,7 +118,7 @@ extern char *config_file;
 
 extern int icmp_sock;
 extern int icmp6_sock;
-extern int ident;
+extern uint16_t ident;
 
 extern struct timeval next_probe;
 
--- src/main.c
+++ src/main.c
@@ -95,7 +95,7 @@ char *config_file=CONFIG;
 
 int icmp_sock;
 int icmp6_sock;
-int ident;
+uint16_t ident;
 
 struct timeval next_probe={0,0};
 
@@ -276,7 +276,7 @@ char *graph_location="/apinger/";
 		return 1;
 	}
 
-	ident=getpid();
+	ident=getpid() & 0xFFFF;
 	signal(SIGTERM,signal_handler);
 	signal(SIGINT,signal_handler);
 	signal(SIGHUP,signal_handler);
