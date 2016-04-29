ed79e2ecc735bf0f25a315b82fe99c25fb95d552
Franco Fichtner <franco@opnsense.org>
2016-04-29 07:10:54 +0200

    don't need tv_macros.h to be public

--- src/apinger.c
+++ src/apinger.c
@@ -46,13 +46,48 @@
 #include <netdb.h>
 
 #include "debug.h"
-#include "tv_macros.h"
 #include "rrd.h"
 
 #ifdef HAVE_ASSERT_H
 # include <assert.h>
 #else
 # define assert(cond)
+#endif
+
+#ifndef timerisset
+# define timerisset(tvp)        ((tvp)->tv_sec || (tvp)->tv_usec)
+#endif
+#ifndef timerclear
+# define timerclear(tvp)        ((tvp)->tv_sec = (tvp)->tv_usec = 0)
+#endif
+#ifndef timercmp
+# define timercmp(a, b, CMP)                                                  \
+  (((a)->tv_sec == (b)->tv_sec) ?                                             \
+   ((a)->tv_usec CMP (b)->tv_usec) :                                          \
+   ((a)->tv_sec CMP (b)->tv_sec))
+#endif
+#ifndef timeradd
+# define timeradd(a, b, result)                                               \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;                          \
+    if ((result)->tv_usec >= 1000000)                                         \
+      {                                                                       \
+        ++(result)->tv_sec;                                                   \
+        (result)->tv_usec -= 1000000;                                         \
+      }                                                                       \
+  } while (0)
+#endif
+#ifndef timersub
+# define timersub(a, b, result)                                               \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                          \
+    if ((result)->tv_usec < 0) {                                              \
+      --(result)->tv_sec;                                                     \
+      (result)->tv_usec += 1000000;                                           \
+    }                                                                         \
+  } while (0)
 #endif
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
